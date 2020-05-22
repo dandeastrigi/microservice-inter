732cbc3baf871c3884db9c14ad7ce30afa442876
Anderson Astrigi <dande.astrigi@gmail.com>
2020-05-22 01:04:27 -0300

    Ajustes na resposta e metodo get

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Node + Redis microservice
 
-To build and start:
+### To build and start:
 
 ```bash
 docker-compose up --build
+```
+
+### Testing:
+Open your browser on http://localhost:4000 or *curl*
+
+```bash
+curl http://localhost:4000
 ```
--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ ig.scrapeUserPage(env.instagramProfile).then(result => {
     setResponse(result.medias);
 }).catch(err => {console.log(err)});
 
+var mediaUrls = [];
 
 /* Print result on screen and save on redis  */
 function setResponse(data){   
@@ -27,11 +28,16 @@ function setResponse(data){
     i = 0;
     data.forEach(function(url) {
         i++;
-        console.log("{key: "+i+",", "url: "+url.display_url+"}")
+        console.log(JSON.stringify({key: i, url: url.display_url}))
+        mediaUrls.push("{key: "+i+",", "url: "+url.display_url+"}")
         client.set(i, url.display_url);
     })
     startApp()
 }
+
+app.get("/", function(req, res) {
+    res.send(mediaUrls)
+})
 
 function startApp(){
     app.listen(env.port, () =>{console.log("Service is up and listen on port "+env.port)})
